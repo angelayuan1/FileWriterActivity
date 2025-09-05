1fcebcedc667e0e69b3ed6b61ec521f03ef580f9
Angela Yuan <ayuan1@hwemail.com>
2025-09-04 20:36:15 -0700

    Adds files, creates file
    Add hidden and non-hidden files using chosen writing method; create .gitignore file

--- MyFileWriter.java
+++ MyFileWriter.java
@@ -1,5 +1,30 @@
+import java.io.*;
+import java.nio.file.*;
+import java.nio.charset.StandardCharsets;
+
 public class MyFileWriter {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+    public static void method1() {
+        String name = ".secret.txt";
+        String password = "password";
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(name))) {
+            bufferedWriter.write(password);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void method2() {
+        String folderName = ".topsecret";
+        String fileName = "confidentialfile.dat";
+        String confidentialInfo = "super top secret confidential info";
+        File hiddenFolder = new File(folderName);
+        hiddenFolder.mkdir();
+        File confidentialFile = new File(hiddenFolder, fileName);
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(confidentialFile))) {
+            bufferedWriter.write(confidentialInfo);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
